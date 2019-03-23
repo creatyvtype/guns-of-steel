7adfcb88dbb77974b521089f0509f069a9b634df
Majd Murad <murad.majd@gmail.com>
2019-03-22 21:42:43 -0400

    gitignore dist

--- packages/common/dist/stores/WorkoutStore.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = require("react");
-var WorkoutStore = /** @class */ (function () {
-    function WorkoutStore() {
-    }
-    return WorkoutStore;
-}());
-exports.WorkoutStoreContext = react_1.createContext(new WorkoutStore());
