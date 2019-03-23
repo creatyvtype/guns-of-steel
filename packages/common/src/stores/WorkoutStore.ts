0d0be85779bb2ca524ecab06cf69a6163e1960ac
Majd Murad <murad.majd@gmail.com>
2019-03-22 21:41:44 -0400

    create workout store

---
+++ packages/common/src/stores/WorkoutStore.ts
@@ -0,0 +1,23 @@
+import { createContext } from 'react';
+type WorkoutDay = 'a' | 'b';
+
+interface WorkoutHistory {
+    [key: string]: Array<{
+        exercise: string;
+        value: number;
+    }>;
+}
+
+class WorkoutStore {
+    currentSquat: number;
+    currentBenchPress: number;
+    currentOverheadPress: number;
+    currentDeadlift: number;
+    currentBarbellRow: number;
+
+    lastWorkoutType: WorkoutDay;
+
+    history: WorkoutHistory;
+}
+
+export const WorkoutStoreContext = createContext(new WorkoutStore());
